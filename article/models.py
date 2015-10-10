e6d155114e25bd38b0315d92ee6d945a30529581
zhaoyun <milo.zhao@qq.com>
2015-10-10 16:48:12 +0800

    rss and paginator

--- article/models.py
+++ article/models.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -6,6 +8,11 @@ class Article(models.Model):
 	category = models.CharField(max_length = 50, blank = True)
 	date_time = models.DateTimeField(auto_now_add = True)
 	content = models.TextField(blank = True, null = True)
+
+	# 获取URL并转换成url的表示格式
+	def get_absolute_url(self):
+		path = reverse('detail', kwargs={'id': self.id})
+		return "http://127.0.0.1:8000%s" % path
 
 	def __unicode__(self):
 		return self.title
